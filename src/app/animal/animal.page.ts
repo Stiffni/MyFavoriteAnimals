5873e1cd4bd7b01ab71b6e15c64adf7ed8aed325
stiffni <steffaniefreeman@gmail.com>
2023-10-11 09:48:22 +0200

    Add favourite to animal page

--- src/app/animal/animal.page.ts
+++ src/app/animal/animal.page.ts
@@ -5,8 +5,12 @@ import { IonicModule } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { Animal } from 'types';
 import { AnimalsAppState } from '../store/reducers/favourites.reducer';
-import { animalDetails } from '../store/selectors/favourites.selectors';
+import {
+  animalDetails,
+  selectAnimalFavouriteStatus,
+} from '../store/selectors/favourites.selectors';
 import { Store } from '@ngrx/store';
+import { toggle } from '../store/actions/favourites.actions';
 
 @Component({
   selector: 'app-animal',
@@ -17,6 +21,19 @@ import { Store } from '@ngrx/store';
 })
 export class AnimalPage {
   animalDetails$: Observable<Animal> = this.store.select(animalDetails);
+  isFavourite$: Observable<boolean>;
+  animalName: string;
 
-  constructor(private store: Store<AnimalsAppState>) {}
+  constructor(private store: Store<AnimalsAppState>) {
+    this.animalDetails$.subscribe((details) => {
+      this.animalName = details.Name;
+    });
+    this.isFavourite$ = this.store.select(
+      selectAnimalFavouriteStatus(this.animalName)
+    );
+  }
+
+  onFavouriteClick() {
+    this.store.dispatch(toggle({ animalName: this.animalName }));
+  }
 }
